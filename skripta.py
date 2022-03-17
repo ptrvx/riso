f2eafa58f1dd1199e7235378dd8eb0acc269ef83
Petar Vukovic <pvukovic@raf.edu.yu>
2022-03-17 18:36:22 +0100

    initial commit

---
+++ skripta.py
@@ -0,0 +1,17 @@
+import http.client
+import json
+
+conn = http.client.HTTPSConnection("jsonplaceholder.typicode.com")
+payload = json.dumps({
+  "userId": "5",
+  "id": "5",
+  "title": "Naslov",
+  "body": "Tekst"
+})
+headers = {
+  'Content-Type': 'application/json'
+}
+conn.request("POST", "/posts", payload, headers)
+res = conn.getresponse()
+data = res.read()
+print(data.decode("utf-8"))
